bf0dc0f00dbc193dd5f5abd2abafd4c616bee479
arthurbraga-plank <arthur.braga@joinplank.com>
2023-04-28 10:47:07 -0300

    Corrige script de dev

---
+++ src/@types/global.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+    readonly PATH_TO_PDF_FILE: string;
+    readonly PATH_TO_SIGNED_PDF_FILE: string;
+    readonly PATH_TO_P12_CERTIFICATE: string;
+    readonly P12_PASSWORD: string;
+    readonly GROUP_ID: string;
+    readonly SIGNATURE_NAME: string;
+    readonly SIGNATURE_LOCATION: string;
+    readonly SIGNATURE_EMAIL: string;
+  }
+}
